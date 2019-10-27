28852ae49343caeff45857f4a34c9a59ec97a657
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-27 13:21:17 +0100

    No steenking static shit!

--- AltDir/Program.cs
+++ AltDir/Program.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrickyUnits;
 
 namespace AltDir {
     class Program {
-        static void Main(string[] args) {
+        private Program(string[] args) {
+            MKL.Lic();
+            MKL.Version();
         }
+
+        void RunMain() { }
+
+        static void Main(string[] args) => (new Program(args)).RunMain(); // Don't want statics, just ignore this line. It's there because C# needs it!
+            
+        
     }
 }
