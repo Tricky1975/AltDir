5925252277e325a72f740ed5c0371a1231153e7f
Jeroen P. Broks <facebook@tbbs.nl>
2019-10-27 13:26:05 +0100

    Ignore trash. It belongs in the trashcan. Not this repository!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+// Lic:
+// Alternate Dir
+// Alternate way to show the directory. Powershell's Get-Item sucks!
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.10.27
+// EndLic
+ï»¿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +33,8 @@ using TrickyUnits;
 namespace AltDir {
     class Program {
         private Program(string[] args) {
-            MKL.Lic();
-            MKL.Version();
+            MKL.Lic    ("Alternate Dir - Program.cs","GNU General Public License 3");
+            MKL.Version("Alternate Dir - Program.cs","19.10.27");
         }
 
         void RunMain() { }
@@ -19,3 +44,4 @@ namespace AltDir {
         
     }
 }
+
